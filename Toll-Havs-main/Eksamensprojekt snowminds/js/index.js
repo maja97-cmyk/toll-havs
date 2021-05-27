2e90122ca311eafe334bca7c3ae736d611101353
Maja Amalie Leonhardt <72253150+maja97-cmyk@users.noreply.github.com>
2021-05-27 10:24:11 +0200

    Add files via upload

---
+++ Toll-Havs-main/Eksamensprojekt snowminds/js/index.js
@@ -0,0 +1,4 @@
+$('document').ready(function() {
+	var options = { videoId: '3H54IVRYdmY', start: 0 };
+	$('#wrapper').tubular(options);
+});
---
+++ Toll-Havs-main/Eksamensprojekt snowminds/js/script.js
@@ -0,0 +1,35 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    //Toggle Nav
+    burger.addEventListener('click', ()=>{
+      nav.classList.toggle('nav-active');
+      
+      //Animate Links
+      navLinks.forEach((link, index)=>{
+        if(link.style.animation){
+          link.style.animation = ''
+        }else{
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+  
+        }
+      });
+      
+      //burger animation
+      burger.classList.toggle('toggle');
+      
+      
+    });
+  }
+
+          /* When the user clicks on the button, 
+        toggle between hiding and showing the dropdown content */
+        function myFunction() {
+          document.getElementById("myDropdown").classList.toggle("show");
+        }
+        
+        
+  navSlide();
+  AOS.init();
